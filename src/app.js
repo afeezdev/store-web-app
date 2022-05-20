f063c4ec0c5b4fd87ace7ba4a191be21b8e0887c
afeezdev <afeezdev@gmail.com>
2022-05-20 13:25:38 +0100

    adding to cart function

--- src/app.js
+++ src/app.js
@@ -33,25 +33,89 @@ function renderProdcuts() {
   }
   renderProdcuts();
 
-  // cart array
-let cart = []
-//  JSON.parse(localStorage.getItem("CART")) || [];
-// updateCart();
+// cart array
+let cart = JSON.parse(localStorage.getItem("CART")) || [];
+updateCart();
+
+// change number of units for an item
+function changeNumberOfUnits(action, id) {
+    cart = cart.map((item) => {
+      let numberOfUnits = item.numberOfUnits;
+  
+      if (item.id === id) {
+        if (action === "minus" && numberOfUnits > 1) {
+          numberOfUnits--;
+        } else if (action === "plus") {
+          numberOfUnits++;
+        }
+      }
+  
+      return {
+        ...item,
+        numberOfUnits,
+      };
+    });
+  
+  }
+
 
 // ADD TO CART
 function addToCart(id) {
-
   // check if prodcut already exist in cart
-//   if (cart.some((item) => item.id === id)) {
-//     changeNumberOfUnits("plus", id);
-//   } else {
-//     const item = items.find((item) => product.id === id);
+  if (cart.some((item) => item.id === id)) {
+    changeNumberOfUnits("plus", id);
+  } else {
+    const item = items.find((product) => product.id === id);
 
-//     cart.push({
-//       ...item,
-//       numberOfUnits: 1,
-//     });
-//   }
-
-//   updateCart();
+    cart.push({
+      ...item,
+      numberOfUnits: 1,
+    });
+  }
+console.log(cart)
 }
+
+// render cart items
+function renderCartItems() {
+    cartItemsEl.innerHTML = ""; // clear cart element
+    cart.forEach((item) => {
+      cartItemsEl.innerHTML += `
+          <div class="cart-item">
+              <div class="item-info" onclick="removeItemFromCart(${item.id})">
+                  <img src="${item.imgSrc}" alt="${item.name}">
+                  <h4>${item.name}</h4>
+              </div>
+              <div class="unit-price">
+                  <small>£</small>${item.price}
+              </div>
+              <div class="units">
+                  <div class="btn minus" onclick="changeNumberOfUnits('minus', ${item.id})">-</div>
+                  <div class="number">${item.numberOfUnits}</div>
+                  <div class="btn plus" onclick="changeNumberOfUnits('plus', ${item.id})">+</div>           
+              </div>
+          </div>
+        `;
+    });
+  }
+
+  // calculate and render subtotal
+function renderSubtotal() {
+    let totalPrice = 0,
+      totalItems = 0;
+  
+    cart.forEach((item) => {
+      totalPrice += item.price * item.numberOfUnits;
+      totalItems += item.numberOfUnits;
+    });
+    subtotalEl.innerHTML = `Subtotal (${totalItems} items): £${totalPrice.toFixed(2)}`;
+  totalItemsInCartEl.innerHTML = totalItems;
+}
+
+
+  function updateCart() {
+    renderCartItems();
+    renderSubtotal();
+  
+     // save cart to local storage
+  localStorage.setItem("CART", JSON.stringify(cart));
+  }
