620d03f44f58c7d4f4290e5a796cde3764a2a814
afeezdev <afeezdev@gmail.com>
2022-05-20 12:46:24 +0100

    add to cart function

--- src/app.js
+++ src/app.js
@@ -1,4 +1,8 @@
 const productsEl = document.querySelector(".products");
+const cartItemsEl = document.querySelector(".cart-items");
+const subtotalEl = document.querySelector(".subtotal");
+const totalItemsInCartEl = document.querySelector(".total-items-in-cart");
+
 
 // RENDER PRODUCTS
 function renderProdcuts() {
@@ -11,7 +15,7 @@ function renderProdcuts() {
                       </div>
                       <div class="desc">
                           <h2>${item.name}</h2>
-                          <h2><small>$</small>${item.price}</h2>
+                          <h2><small>£</small>${item.price}</h2>
                           <p>
                               ${item.special_offer}
                           </p>
@@ -28,3 +32,26 @@ function renderProdcuts() {
     });
   }
   renderProdcuts();
+
+  // cart array
+let cart = []
+//  JSON.parse(localStorage.getItem("CART")) || [];
+// updateCart();
+
+// ADD TO CART
+function addToCart(id) {
+
+  // check if prodcut already exist in cart
+//   if (cart.some((item) => item.id === id)) {
+//     changeNumberOfUnits("plus", id);
+//   } else {
+//     const item = items.find((item) => product.id === id);
+
+//     cart.push({
+//       ...item,
+//       numberOfUnits: 1,
+//     });
+//   }
+
+//   updateCart();
+}
--- src/items.js
+++ src/items.js
@@ -1,30 +1,30 @@
 const items = [
     {
-      id: 1,
+      id: 0,
       name: "A",
       price: 0.50,
-      special_offer: "3 for 130",
+      special_offer: "3 for £1.30",
       imgSrc: "./img/item A.jpg",
     },
     {
-        id: 2,
+        id: 1,
         name: "B",
         price: 0.30,
-        special_offer: "2 for 45",
+        special_offer: "2 for £0.45",
         imgSrc: "./img/item B.jpg",
       },
       {
-        id: 3,
+        id: 2,
         name: "C",
         price: 0.20,
-        special_offer: "3 for 50",
+        special_offer: "3 for £0.50",
         imgSrc: "./img/item C.webp",
       },
       {
-        id: 4,
+        id: 3,
         name: "D",
         price: 0.15,
-        special_offer: "2 for 25",
+        special_offer: "2 for £0.25",
         imgSrc: "./img/item D.png",
       },
       
