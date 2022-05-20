addd1bda561b852770b6115d20fe0773f7a2dd66
afeezdev <afeezdev@gmail.com>
2022-05-20 11:09:28 +0100

    highligting the items in the store

---
+++ src/item.js
@@ -0,0 +1,34 @@
+const items = [
+    {
+      id: 1,
+      name: "A",
+      price: 50,
+      special_offer: "3 for 130",
+      imgSrc: "./img/item A.jpg",
+    },
+    {
+        id: 2,
+        name: "B",
+        price: 30,
+        special_offer: "2 for 45",
+        imgSrc: "./img/item B.jpg",
+      },
+      {
+        id: 3,
+        name: "C",
+        price: 20,
+        special_offer: "3 for 50",
+        imgSrc: "./img/item C.webp",
+      },
+      {
+        id: 4,
+        name: "D",
+        price: 15,
+        special_offer: "2 for 25",
+        imgSrc: "./img/item D.png",
+      },
+      
+
+
+  ];
+  
